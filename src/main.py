595738812cabaaef190cf630f89541f0674be4ce
zultopia <117258762+zultopia@users.noreply.github.com>
2024-03-12 22:20:29 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 n = int(input("Masukkan banyaknya titik: "))
 points = [tuple(map(float, input(f"Masukkan koordinat titik P{i} (pisahkan dengan spasi): ").split())) for i in range(n)]
 iterations = int(input("Masukkan jumlah iterasi: "))
-'''
+
 # Divide and conquer Execution Time
 start_time = time.time()
 curve_points_dc = dnc.bezier_dnc(n, points, iterations)
 print(curve_points_dc)
 end_time = time.time()
 print("Divide and Conquer Approach Time:", end_time - start_time , "ms")
-'''
+
 # Brute force Execution Time
 start_time = time.time()
 curve_points_bf = bf.bezier_bf(points)
@@ -26,5 +26,5 @@ end_time = time.time()
 print("Brute Force Approach Time:", end_time - start_time, "ms")
 
 # Visualisasi Kurva
-# plot_curve(curve_points_dc)
+plot_curve(curve_points_dc)
 plot_curve(curve_points_bf)
